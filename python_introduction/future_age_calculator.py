0dae0583bd0152a417177da6a0083be93baf7426
Enohense <williamssandra803@gmail.com>
2025-07-13 22:36:33 +0100

    Add area if rectangle, convert hours to seconds and future age calculation

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,11 @@
+# Prompt user for input
+current_age = int(input("How old are you? "))
+
+# Calculate age in 2050
+years_to_2050 = 2050 - 2023
+future_age = current_age + years_to_2050
+
+# Print result
+print(f"In 2050, you will be {future_age} years old.")
+
+
