d16bd61cda3bbf1a710f9fe5f1e39ad34fbc2363
Enohense <williamssandra803@gmail.com>
2025-07-20 04:38:10 +0100

    ALX python task

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -6,19 +6,17 @@ time_bound = input("Is it time-bound? (yes/no): ").strip().lower()
 
 match priority:
     case "high":
-        message = f"Reminder: '{task}' is a high priority task."
+        message = f"Reminder: '{task}' is a high priority task"
     case "medium":
-        message = f"Note: '{task}' is a medium priority task."
+        message = f"Note: '{task}' is a medium priority task"
     case "low":
-        message = f"Note: '{task}' is a low priority task."
+        message = f"Note: '{task}' is a low priority task"
     case _:
         message = f"'{task}' has an unspecified priority"
 
 if time_bound == "yes":
     message += " that requires immediate attention today!"
-elif time_bound == "no":
-    message += " Consider completing it when you have free time."
 else:
-    message += ". Enter a valid time bound."
+    message += ". Consider completing it when you have free time."
 
-print(f" {message}")
+print(message)
